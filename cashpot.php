f2ab35dc29351c0997fd68062754636426b7fa2e
Niren Orie <nireno@gmail.com>
2011-07-27 20:44:39 -0400

    Modified the regex pattern to use subpatterns

--- cashpot.php
+++ cashpot.php
@@ -4,12 +4,12 @@
 //echo date('dmy');
 $url = "http://www.nlcb.co.tt/search/cpq/cashQuery.php";
 $fields = array('day'=> '01', 'month'=> 'Jan', 'year'=> '11');
-$r = new HttpRequest($url, HttpRequest::METH_POST);
-$r->addPostFields($fields);
+$request = new HttpRequest($url, HttpRequest::METH_POST);
+$request->addPostFields($fields);
 try
 {
-	$r->send();
-	if($r->getResponseCode() == 200)  $cashpot_html = $r->getResponseBody();
+	$request->send();
+	if($request->getResponseCode() == 200)  $response = $request->getResponseBody();
 	else echo "Request Failed";
 }
 catch(HttpException $e)
@@ -17,33 +17,38 @@ catch(HttpException $e)
 	die($e);
 }
 
-$pattern = '/;\s*.*?</';
-preg_match_all($pattern, $cashpot_html, $all_matches);
-$matches = $all_matches[0];
-$draw_number = clean_regex_match($matches[0]);
-$date = clean_regex_match($matches[1]);
-$numbers = clean_regex_match($matches[2]);
-$multiplier = clean_regex_match($matches[3]);
-$jackpot = clean_regex_match($matches[4]);
-$winners = clean_regex_match($matches[5]);
+$draw_data = extract_draw_data($response);
+$draw_number = $draw_data[0];
+$date = $draw_data[1];
+$numbers = $draw_data[2];
+$multiplier = $draw_data[3];
+$jackpot = $draw_data[4];
+$winners = $draw_data[5];
 
 
 print_draw_result($draw_number, $date, $numbers, $multiplier, $jackpot, $winners);
 
-function clean_regex_match($match)
+function extract_draw_data($html)
 {
-	return trim(substr($match, 1, strlen($match) - 2));
+	$pattern = '/;\s*(.*?)</';
+	preg_match_all($pattern, $html, $all_matches);
+	$matches = $all_matches[1]; //Get the matches for the subpattern (.*?) only
+	for($i = 0; $i < 6; $i++)
+	{
+		$matches[$i] = trim($matches[$i]);
+	}
+	return $matches;
 }
 
 function print_draw_result($draw_number, $date, $numbers, $multiplier, $jackpot, $winners)
 {
 	echo <<<RESULT
-Draw #:     $draw_number
-Date:       $date
-Numbers:    $numbers
-Multiplier: $multiplier
-Jackpot:    $jackpot
-Winners:    $winners
+Draw #:     $draw_number$
+Date:       $date$
+Numbers:    $numbers$
+Multiplier: $multiplier$
+Jackpot:    $jackpot$
+Winners:    $winners$
 
 RESULT;
 }
